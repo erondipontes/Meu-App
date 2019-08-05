00787771e6a55c622ddd451f708ea0db351c5b99
erondipontes <erondipontes@hotmail.com>
2019-08-06 08:01:28 +0900

    Botão de vibrar e ler código inserido.

--- www/components/acao.js
+++ www/components/acao.js
@@ -17,4 +17,32 @@ $(document).on("click","#alerta",function(){
   //navigator.notification.beep(2);
   navigator.notification.confirm("Escolha uma Opçao",retorno2,"Titulo",['Sim','Não']);
 });
-
+$(document).on("click","#vibrar",function(){
+  navigator.vibrate(3000);
+});
+$(document).on("click","#LerCodigo",function(){
+  cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("Resultado da Leitura\n" +
+                "Resultado: " + result.text + "\n" +
+                "Formato: " + result.format + "\n" +
+                "Cancelado: " + result.cancelled);
+      },
+      function (error) {
+          alert("Falha no Scanning : " + error);
+      },
+      {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: false, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Coloque um código de barras dentro da área de digitalização", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417,CODE_39", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+   );
+});
